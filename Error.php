d7d5f1212ff387ae0a23037eacd718686276fa4f
MGDavid01 <0322013760@ut-tijuana.edu.mx>
2024-11-07 17:04:07 -0800

    prueba2

--- Error.php
+++ Error.php
@@ -8,7 +8,7 @@
 <body>
     <section>
         <div>
-            <h2>Correo Registrado</h2>
+            <h2>Email Register</h2>
             <p>Esta cuennta de correo Electronico ya esta asociada a una empresa</p>
         </div>
         <div>
